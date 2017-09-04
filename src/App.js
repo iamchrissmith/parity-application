0c4454e408cf43dce612d958d0226b9c93c8f1d4
Chris Smith <42@chrisryansmith.com>
2017-09-04 09:44:20 -0600

    setup main App js and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+
+import 'semantic-ui-css/semantic.css';
 import './App.css';
+
 import job from './resources/javascript_advert';
 import Summary from './components/Summary';
+import Map from './components/Map';
 import Methodology from './components/Methodology';
 import Details from './components/Details';
 import Requirements from './components/Requirements';
@@ -12,7 +16,8 @@ class App extends Component {
     return (
       <div className="App">
         <Summary headline={job.headline} essentials={job.essentials} />
-        <Methodology />
+        <Map />
+        <Methodology methodology={job.methodology} />
         <Details />
         <Requirements />
         <MoreInfo />
